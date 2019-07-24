c37c36f067c50668e91f77b1a38a441e0f32b100
Bhoopendra <bhaskarbhoopendra8@gmail.com>
2019-07-24 23:59:20 +0530

    backend url change

--- controllers/product.js
+++ controllers/product.js
@@ -5,7 +5,9 @@ const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.productById = (req, res, next, id) => {
-    Product.findById(id).exec((err, product) => {
+    Product.findById(id)
+    .populate('category') 
+    .exec((err, product) => {
         if (err || !product) {
             return res.status(400).json({
                 error: "Product not found"
@@ -30,7 +32,7 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
-        // check for all fields
+       
         const {
             name,
             description,
@@ -55,11 +57,10 @@ exports.create = (req, res) => {
 
         let product = new Product(fields);
 
-        // 1kb = 1000
-        // 1mb = 1000000
+       
 
         if (files.photo) {
-            // console.log("FILES PHOTO: ", files.photo);
+            
             if (files.photo.size > 1000000) {
                 return res.status(400).json({
                     error: "Image should be less than 1mb in size"
@@ -103,7 +104,7 @@ exports.update = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
-        // check for all fields
+       
         const {
             name,
             description,
@@ -129,8 +130,7 @@ exports.update = (req, res) => {
         let product = req.product;
         product = _.extend(product, fields);
 
-        // 1kb = 1000
-        // 1mb = 1000000
+        
 
         if (files.photo) {
             // console.log("FILES PHOTO: ", files.photo);
@@ -154,12 +154,7 @@ exports.update = (req, res) => {
     });
 };
 
-/**
- * sell / arrival
- * by sell = /products?sortBy=sold&order=desc&limit=4
- * by arrival = /products?sortBy=createdAt&order=desc&limit=4
- * if no params are sent, then all products are returned
- */
+
 
 exports.list = (req, res) => {
     let order = req.query.order ? req.query.order : "asc";
@@ -181,10 +176,6 @@ exports.list = (req, res) => {
         });
 };
 
-/**
- * it will find the products based on the req product category
- * other products that has the same category, will be returned
- */
 
 exports.listRelated = (req, res) => {
     let limit = req.query.limit ? parseInt(req.query.limit) : 6;
@@ -213,14 +204,6 @@ exports.listCategories = (req, res) => {
     });
 };
 
-/**
- * list products by search
- * we will implement product search in react frontend
- * we will show categories in checkbox and price range in radio buttons
- * as the user clicks on those checkbox and radio buttons
- * we will make api request and show the products to users based on what he wants
- */
-
 exports.listBySearch = (req, res) => {
     let order = req.body.order ? req.body.order : "desc";
     let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
@@ -228,14 +211,10 @@ exports.listBySearch = (req, res) => {
     let skip = parseInt(req.body.skip);
     let findArgs = {};
 
-    // console.log(order, sortBy, limit, skip, req.body.filters);
-    // console.log("findArgs", findArgs);
-
     for (let key in req.body.filters) {
         if (req.body.filters[key].length > 0) {
             if (key === "price") {
-                // gte -  greater than price [0-10]
-                // lte - less than
+                
                 findArgs[key] = {
                     $gte: req.body.filters[key][0],
                     $lte: req.body.filters[key][1]
@@ -264,6 +243,17 @@ exports.listBySearch = (req, res) => {
             });
         });
 };
+// exports.init =(req,res,next)=>{
+//     // creating the energy here to send the data
+//     const data= {};
+//     data.data[0].then(response=>{
+//         if(response.error){
+//             setError{
+
+//             }
+//         }
+//     })
+// }
 
 exports.photo = (req, res, next) => {
     if (req.product.photo.data) {
@@ -274,17 +264,16 @@ exports.photo = (req, res, next) => {
 };
 
 exports.listSearch = (req, res) => {
-    // create query object to hold search value and category value
+  
     const query = {};
-    // assign search value to query.name
+    
     if (req.query.search) {
         query.name = { $regex: req.query.search, $options: "i" };
-        // assigne category value to query.category
+        
         if (req.query.category && req.query.category != "All") {
             query.category = req.query.category;
         }
-        // find the product based on query object with 2 properties
-        // search and category
+       
         Product.find(query, (err, products) => {
             if (err) {
                 return res.status(400).json({
@@ -295,3 +284,5 @@ exports.listSearch = (req, res) => {
         }).select("-photo");
     }
 };
+
+
